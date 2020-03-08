bc89f06e6f3d2b955235f529bccffe7444de2253
Jim Kang <jimkang@gmail.com>
2020-03-08 13:03:03 -0400

    Notify the player when they're being taken.

--- flows/commands/take-command.ts
+++ flows/commands/take-command.ts
@@ -1,4 +1,5 @@
 import { CmdParams, Soul } from '../../types';
+var findWhere = require('lodash.findwhere');
 
 export function takeCmd({ gameState, targetTree, cmd }: CmdParams) {
   var items: Array<Soul> = cmd.targets;
@@ -6,6 +7,10 @@ export function takeCmd({ gameState, targetTree, cmd }: CmdParams) {
   if (items) {
     cmd.actor.items = cmd.actor.items.concat(items);
     gameState.soulTracker.removeSouls(targetTree, items);
+
+    if (findWhere(items, { id: 'player' })) {
+      gameState.displayMessage = `Uh, oh! A ${cmd.actor.type} has taken you! I hope it drops you soon! (You can hit reload if are really not OK with this.)`;
+    }
   } else {
     throw new Error('Somehow no item to take.');
   }
